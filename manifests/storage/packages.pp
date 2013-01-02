c15fb2ecaffe0bf4101ee6eaaf6596807789c683
Jelle Smet <jelle.smet@tomtom.com>
2013-01-02 13:39:51 +0100

    some finetuning

--- manifests/storage/packages.pp
+++ manifests/storage/packages.pp
@@ -1,20 +1,23 @@
 class graphite::storage::packages(){
   file { "/etc/init.d/carbon-cache":
-    mode => 540,
+    mode => 664,
     owner => root,
     group => root,
     source => "puppet:///modules/graphite/carbon-cache.init",
     require => Class [ "graphite::packages" ]
   } ->
   file { "/etc/sysconfig/carbon-cache":
-    mode => 540,
+    mode => 664,
     owner => root,
     group => root,
     source => "puppet:///modules/graphite/carbon-cache.sysconfig",
     require => Class [ "graphite::packages" ]
   }
+  file { "/etc/carbon":
+    mode => 755
+  }
   file { "/etc/carbon/storage-aggregation.conf":
-    mode => 540,
+    mode => 664,
     owner => root,
     group => root,
     source => "puppet:///modules/graphite/storage-aggregation.conf",
